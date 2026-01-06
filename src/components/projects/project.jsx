3128566d40e4c119f48fc50058308fb73a4260fb
tejasvising <singhtejasvi.2001@gmail.com>
2026-01-06 21:43:52 +0530

    project tab fixed

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -5,31 +5,26 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles/project.css";
 
-const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
-
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link}>
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-
-							<div className="project-link-text">{linkText}</div>
-						</div>
-					</div>
-				</Link>
-			</div>
-		</React.Fragment>
-	);
+const Project = ({ logo, title, description, linkText, link }) => {
+    return (
+        <div className="project">
+            <Link to={link} target="_blank" rel="noopener noreferrer">
+                <div className="project-container">
+                    <div className="project-logo">
+                        <img src={logo} alt={`${title} logo`} />
+                    </div>
+                    <div className="project-title">{title}</div>
+                    <div className="project-description">{description}</div>
+                    <div className="project-link">
+                        <div className="project-link-icon">
+                            <FontAwesomeIcon icon={faLink} />
+                        </div>
+                        <div className="project-link-text">{linkText}</div>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    );
 };
 
 export default Project;
